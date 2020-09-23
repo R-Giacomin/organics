fc0783ce1e7ee2cf454612652758edf2f56127a7
gabriellaneves <gabriella.nunes@gmail.com>
2020-09-23 14:19:02 -0300

    Create and destroy cart_products controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :products do
+    resources :cart_products, only: [:create]
+  end
+
+  resources :cart_products, only: [:destroy]
 end
