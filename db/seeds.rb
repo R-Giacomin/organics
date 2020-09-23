723294f2dfafad174c18b85de166dae97dae225a
Rafael Giacomin <faellg@gmail.com>
2020-09-23 16:42:08 -0300

    index with itens search

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.new(name: "banana maça", description:"Kg", price: 6, quantity: 3 )
+product.user = User.find(1)
+product.save
+product = Product.new(name: "banana caturra", description:"Kg", price: 5, quantity: 5 )
+product.user = User.find(1)
+product.save
+product = Product.new(name: "maça gala", description:"Kg", price: 5, quantity: 5 )
+product.user = User.find(1)
+product.save
+product = Product.new(name: "maça julieta", description:"Kg", price: 5, quantity: 5 )
+product.user = User.find(1)
+product.save
