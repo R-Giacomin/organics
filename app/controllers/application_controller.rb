e6980329d169034330d1af02121f7e82e2d14636
gabriellaneves <gabriella.nunes@gmail.com>
2020-09-28 14:49:55 -0300

    implementação foto e meu perfil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :category, :address, :email, :password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :category, :address, :email, :password, :photo)}
     # devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :category, :address, :email, :password, :photo)}
   end
 end
